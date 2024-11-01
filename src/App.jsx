a7908f2728606d698e82b383e7bf999f5127723c
ayesharashid11 <bscs2211@gmail.com>
2024-11-01 23:53:58 +0500

    player values  to boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-
+  const[box, setBox] = useState(Array(9).fill(null));
+const [player, setPlayer]= useState(false);
+const handleToggle = (index)=>{
+  const newBox = box.slice();
+  newBox[index] = player ? 'X' : 'O';
+  setBox(newBox);
+  setPlayer(!player);
+}
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-950 to-purple-900">
-      <div className="text-2xl font-semibold text-white mb-6">Player</div>
+      <div className="text-2xl font-semibold text-white mb-6">Player {player? 'X' : 'O' }</div>
       <div className="grid grid-cols-3 gap-2 p-4 bg-white bg-opacity-20 backdrop-blur-md rounded-lg shadow-lg">
-       
-          <button
-            className="h-20 w-20 bg-white bg-opacity-20 text-3xl font-bold flex items-center justify-center text-white rounded-lg"
-          >
-         X
-          </button>
-       
+       {
+        box.map((value, index)=>(
+          <button key={index} onClick={() => handleToggle(index)} disabled={value}
+          className="h-20 w-20 bg-white bg-opacity-20 text-3xl font-bold flex items-center justify-center text-white rounded-lg"
+        >
+   {value}
+        </button>
+        ))
+       }
       </div>
     </div>
   )
